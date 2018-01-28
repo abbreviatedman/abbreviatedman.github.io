a8c7737518f12a0df14531ce164808e470f266a8
Colin Jaffe <balloonasaurus@gmail.com>
2018-01-28 10:10:41 -0500

    adds full Vue

---
+++ data.js
@@ -0,0 +1,17 @@
+var data = {
+  diner: {
+    title: 'Diner',
+    copy: 'The Redux Restaurant',
+    imgSrc: './images/diner.jpg'
+  },
+  bisque: {
+    title: 'Bisque',
+    copy: 'A delightful productivity melange.',
+    imgSrc: './images/bisque.jpg'
+  },
+  gameOfLeaf: {
+    title: 'Game of Leaf',
+    copy: 'Conway\'s Game of Life in Elm',
+    imgSrc: './images/game-of-leaf.jpg'
+  }
+};
