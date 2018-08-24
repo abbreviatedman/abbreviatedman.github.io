046756402c6d11064fa06a769c20efd6bf6116d0
abbreviatedman <colin.jaffe@gmail.com>
2018-08-24 14:09:57 -0400

    removes cors error in firefox browser

--- js/appMethods.js
+++ js/appMethods.js
@@ -25,22 +25,8 @@ var appMethods = {
       baseURL: 'https://icanhazdadjoke.com/',
       headers: {
         Accept: 'application/json',
-      }
+      },
     };
-
-    /*
-      The icanhazdadjokes API kindly requests
-      user-agent info, but Chromium-based browsers
-      will console.error if you set 'User-Agent'
-      yourself.
-
-      The request will still go through, but I'd
-      rather avoid red console text if I can.
-    */
-    const siteDescription = 'colinjaffe.com, a personal website, colin.jaffe@gmail.com'
-    if (this.isChromium()) {
-      request.headers['User-Agent'] = siteDescription;
-    }
 
     try {
       const response = await axios(request);
@@ -48,19 +34,5 @@ var appMethods = {
     } catch (error) {
       console.log(error);
     }
-  },
-
-  isChromium: function() {
-    const isChromium = window.chrome;
-    const winNav = window.navigator;
-    const vendorName = winNav.vendor;
-    const isOpera = typeof window.opr !== "undefined";
-    const isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-
-    return isChromium !== null &&
-    typeof isChromium !== "undefined" &&
-    vendorName === "Google Inc." &&
-    isOpera === false &&
-    isIEedge === false
   }
 };
